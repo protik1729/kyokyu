7012361af0c827d20dd931860b12d7301475c537
protik1729 <60181951+protik1729@users.noreply.github.com>
2020-05-19 15:58:20 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset($_POST['submit'])) {
+if(isset($_POST['submit'])) {
   $name= $_POST['name'];
   $subject = $_POST['subject'];
   $mailFrom=$_POST['mail'];
@@ -10,8 +10,12 @@ if (isset($_POST['submit'])) {
   $headers="From: ".$mailFrom;
   $txt="You have received an email from ".$name.".\n\n".$message;
 
-  mail($mailTo, $subject, $txt, $headers);
-  header("Location: index.php?mailsend");
+  if(mail($mailTo, $subject, $txt, $headers)){
+      echo "<p>sent successfully!<p> ";
+  }
+  else {
+    echo "something went wrong";
+  }
 }
 
 ?>
