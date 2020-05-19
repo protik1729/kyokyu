756d8cb7def757b532488744cec475d3532ade95
protik1729 <60181951+protik1729@users.noreply.github.com>
2020-05-19 12:29:51 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])) {
   $mailFrom=$_POST['mail'];
   $message=$_POST['message'];
 
-  $mailTo="pratikpawar45678@outlook.com";
+  $mailTo="pratikpawar45678@gmail.com";
   $headers="From: ".$mailFrom;
   $txt="You have received an email from ".$name.".\n\n".$message;
 
